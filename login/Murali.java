c1cd5005d9ca1c612cd11771d06679fe3203799a
murali <murali@gmail.com>
2024-02-18 12:42:31 +0530

    html code

---
+++ login/Murali.java
@@ -0,0 +1,21 @@
+import java.lang.ClassNotFoundException;
+
+class MultithreadingDemo extends Thread {
+    public void run() {
+        try {
+            System.out.println("Thread" + Thread.currentThread().getName() + "is running");
+        } catch (Exception e) {
+            System.out.println("Exception is caught");
+        }
+    }
+}
+
+class Murali {
+    public static void main(String args[]) {
+        int n = 8;
+        for (int i = 0; i < n; i++) {
+            MultithreadingDemo obj = new MultithreadingDemo();
+            obj.start();
+        }
+    }
+}
